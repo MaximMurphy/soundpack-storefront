62093b58c1fda9261a9db079d25bee02deb2d870
Maxim Murphy <maxim.murphy8@gmail.com>
2023-05-12 15:24:52 -0400

    prevent mouse tracker from extending page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,26 @@ import { Icon } from "@iconify/react";
 
 export function HomePageText() {
   return (
-    <main className="w-full min-h-screen flex flex-col items-center dark:text-stone-400">
-      <h1 className="text-3xl lg:text-5xl font-mediumtracking-wide antialiased text-center px-12 pt-20 lg:px-64 lg:pt-36">
+    <main className="w-full h-full flex flex-col items-center dark:text-stone-400">
+      <h1 className="text-3xl lg:text-5xl font-medium tracking-wide antialiased text-center px-12 pt-20 lg:px-64 lg:pt-36">
         <span className="font-semibold">Unique</span> and{" "}
         <span className="font-semibold">Innovative</span> sounds for your dream
         electronic music production
       </h1>
       <p className="text-lg lg:text-xl font-medium tracking-wide antialiased text-center px-12 pt-8 lg:px-64 lg:pt-20">
-        Browse the collection of Electronic, House, & Dance sample packs, synth
-        sounds, and serum presets.
+        Browse the collection of sample packs, synth sounds, and serum presets.
       </p>
+      <p className="hidden md:flex text-lg lg:text-xl font-medium tracking-wide antialiased text-center px-12 lg:px-64">
+        Perfect for any Electronic, House, Dance, or Pop track.
+      </p>
+
       <div className="flex flex-row p-8 gap-8">
-        <button>Browse</button>
-        <div className="flex flex-row">
-          <button>Learn More </button>
-          <Icon icon="ic:sharp-arrow-right-alt" className="h-6 w-6 mt-1" />
+        <button className="font-bold bg-blue-800 p-4 rounded-2xl text-white">
+          Shop Sounds
+        </button>
+        <div className="flex flex-row items-center">
+          <button className="font-bold">Learn More </button>
+          <Icon icon="ic:sharp-arrow-right-alt" className="h-6 w-6" />
         </div>
       </div>
     </main>
@@ -33,13 +38,17 @@ export default function Home() {
   if (window.matchMedia("(min-width: 701px)").matches) {
     //desktop
     return (
-      <>
+      <div className="overflow-hidden">
         <HomePageText />
         <Blob />
-      </>
+      </div>
     );
   } else {
     //mobile
-    return <HomePageText />;
+    return (
+      <div className="overflow-hidden">
+        <HomePageText />
+      </div>
+    );
   }
 }
--- src/components/Blob.tsx
+++ src/components/Blob.tsx
@@ -11,7 +11,7 @@ export default function Blob() {
           left: `${clientX}px`,
           top: `${clientY}px`,
         },
-        { duration: 3000, fill: "forwards" }
+        { duration: 2000, fill: "forwards" }
       );
     };
   }, []);
@@ -19,7 +19,7 @@ export default function Blob() {
   return (
     <div
       id="blob"
-      className="bg-gradient-to-r from-blue-600/10 to-fuchsia-600/10 h-[35rem] w-[35rem] absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] rounded-full blur-[200px]"
+      className="-z-10 bg-gradient-to-r from-blue-600/20 to-fuchsia-600/20 h-[35rem] w-[35rem] fixed left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] rounded-full blur-[200px]"
     ></div>
   );
 }
