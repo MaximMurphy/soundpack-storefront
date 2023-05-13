813920b72350d6f74e4a5b36627f5151d7e9af8c
Maxim Murphy <maxim.murphy8@gmail.com>
2023-05-13 19:01:15 -0400

    Created layout for individual product page

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -5,7 +5,9 @@ import Link from "next/link";
 const Products = ({ products }: { products: Array<any> }) => {
   return (
     <div className="-mt-24 w-full h-full px-12 lg:px-24 pt-12 pb-36 lg:pb-48 bg-stone-200 dark:bg-stone-800">
-      <h1 className="mt-24 text-2xl lg:text-3xl font-semibold">Products</h1>
+      <h1 className="mt-24 text-2xl lg:text-3xl font-semibold tracking-wide">
+        Products
+      </h1>
       <div className="mt-8 grid grid-cols-1 gap-y-8 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8">
         {products.map((product: any) => (
           <Link
@@ -23,8 +25,10 @@ const Products = ({ products }: { products: Array<any> }) => {
                 className="group-hover:opacity-75"
               />
             </div>
-            <h3 className="mt-1 text-base font-medium">{product.name}</h3>
-            <p className="mt-1 text-sm font-medium">
+            <h3 className="mt-2 text-base font-semibold tracking-wide">
+              {product.name}
+            </h3>
+            <p className="mt-1 text-sm font-semibold tracking-wide">
               {formatCurrency({ amount: product.price })}
             </p>
           </Link>
