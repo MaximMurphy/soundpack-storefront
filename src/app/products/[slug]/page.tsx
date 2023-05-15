85e7ef1f460cd4151628fefef751a8a06a96a97c
Maxim Murphy <maxim.murphy8@gmail.com>
2023-05-15 13:11:33 -0400

    Add page metadata

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,6 +1,11 @@
 import Product from "@/components/product";
 import { getProductBySlugOrId } from "../../../../lib/swell/products";
 
+export const metadata = {
+  title: "WAVFORM",
+  description: "Electronic Music Sounds and Presets",
+};
+
 const Page = async ({ params }: { params: any }) => {
   const product = await getProductBySlugOrId(params.slug);
 
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,11 @@
 import Products from "@/components/products";
 import { getProducts } from "../../../lib/swell/products";
 
+export const metadata = {
+  title: "Products",
+  description: "Electronic Music Sounds and Presets",
+};
+
 const Page = async () => {
   const { results: products } = await getProducts({ page: 1 });
 
