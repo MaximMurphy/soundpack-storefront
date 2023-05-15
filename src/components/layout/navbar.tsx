0701eee919747bba9c24025e5768d1fdf76728ac
Maxim Murphy <maxim.murphy8@gmail.com>
2023-05-15 12:51:41 -0400

    Add new WAVFORM logo

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import useSWR from "swr";
 import Link from "next/link";
+import Image from "next/image";
 import { Icon } from "@iconify/react";
 import DarkModeBtn from "../DarkModeBtn";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
@@ -23,11 +24,15 @@ export default function Navbar() {
           <span className="bg-fuchsia-800 w-full"> </span>
         </div>
         <Link href="/" className="flex text-center justify-center">
-          <p className="w-72 text-3xl font-bold pt-4 hover:underline decoration-1 underline-offset-2">
-            WAVFORM
-          </p>
+          <Image
+            src="/wavform.png"
+            alt="WAVFORM"
+            height={229}
+            width={1090}
+            className="h-full w-36 lg:w-48 mt-4 dark:invert"
+          />
         </Link>
-        <div className="flex flex-row text-center justify-between px-4 lg:px-8 pb-8 -mt-8">
+        <div className="flex flex-row text-center justify-between px-4 lg:px-8 pb-8 -mt-[1.75rem] lg:-mt-10">
           <div className="flex flex-row text-md font-semibold lg:text-lg gap-4 lg:gap-8">
             <Link
               href="/products"
